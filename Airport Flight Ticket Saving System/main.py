7db525e8a8e0e17866d288054f3150c65edae13e
devanrio <devanrio2006@gmail.com>
2020-09-02 19:44:38 +0700

    Hasil Project

---
+++ Airport Flight Ticket Saving System/main.py
@@ -0,0 +1,51 @@
+from json import load, dump
+from os import system
+from time import sleep
+
+import feature
+
+statusLoading = feature.loadData()
+
+if statusLoading :
+	passLogin = feature.login()
+	if passLogin:
+		print('Welcome!')
+		sleep(2)
+		menu_choice = ''
+
+		while menu_choice != 'e':
+			system('cls')
+			feature.print_menu()
+			menu_choice = input('Type in a code : ').lower()
+
+			if menu_choice == '1':
+				feature.print_scheduled_ticket()
+				input('ENTER to EXIT')
+
+			elif menu_choice == '2':
+				feature.schedule_ticket()
+				input('ENTER to EXIT')
+
+			elif menu_choice == '3':
+				feature.remove_ticket()
+				input('ENTER to EXIT')
+
+			elif menu_choice == '4':
+				feature.lookup_ticket()
+				input('ENTER to EXIT')
+
+			elif menu_choice == '5':
+				feature.print_pdf()
+				input('ENTER to EXIT')
+			elif menu_choice == 'e':
+				break
+			else:
+				print('Input Menu Choice Correctly')
+	else:
+		print('Failed to login')
+
+else :
+	print('Apps cannot run.')	
+
+
+
